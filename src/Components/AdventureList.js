5baf5f4d610134a316828188736d67611472da4d
kalbek <access2kaleb@gmail.com>
2023-09-04 07:01:23 +0300

    Implement user redirect to login page from adventrue list page if not authenticated.

--- src/Components/AdventureList.js
+++ src/Components/AdventureList.js
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import fetchAdventuresData from '../redux/adventureActions';
-import Adventure from './Adventure';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import fetchAdventuresData from "../redux/adventureActions";
+import Adventure from "./Adventure";
 
 const AdventureList = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const adventures = useSelector((state) => state.adventures.adventures);
+
+  // Check if the user is authenticated
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+
+  // Fetch adventure data from the server on component mount
   useEffect(() => {
     dispatch(fetchAdventuresData());
   }, [dispatch]);
+
+  // Redirect to the login page if the user is not authenticated
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isAuthenticated]);
 
   return (
     <>
@@ -25,6 +39,7 @@ const AdventureList = () => {
           ))}
         </div>
       )}
+      {/* Display a message if there are no adventures */}
       {adventures.length === 0 && <h3>There are no adventures!</h3>}
     </>
   );
