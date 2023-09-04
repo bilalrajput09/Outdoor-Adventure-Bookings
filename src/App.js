fbc6a71aefa12c52ac9ba9fadcfa757cefb5ffd4
kalbek <access2kaleb@gmail.com>
2023-09-04 06:57:22 +0300

    Update App.js routing and layout based on user authentication.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import './App.css';
-import AdventureList from './Components/AdventureList';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Routes } from "react-router-dom";
+import AdventureList from "./Components/AdventureList";
+import Reservations from "./Components/Reservations";
+import RegisterForm from "./Components/RegisterForm";
+import LoginForm from "./Components/LoginForm";
+import Layout from "./Components/Layout";
+import "./App.css";
 
 function App() {
+  // Check if the user is authenticated
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+
   return (
     <>
-      <div className="container">
-        <AdventureList />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<RegisterForm />} />
+          {isAuthenticated ? (
+            <Route element={<Layout />}>
+              <Route path="/" element={<AdventureList />} />
+              <Route path="/adventures" element={<AdventureList />} />
+              <Route path="/reservations" element={<Reservations />} />
+            </Route>
+          ) : (
+            <>
+              <Route path="/" element={<LoginForm />} />
+            </>
+          )}
+        </Routes>
+      </Router>
     </>
   );
 }
