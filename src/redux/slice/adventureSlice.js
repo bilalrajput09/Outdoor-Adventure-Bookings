f60b277d2f3bf10535e6c1005dbfaa4fa45b03db
kalbek <access2kaleb@gmail.com>
2023-09-05 05:57:56 +0300

    Add createAdventure to adventureSlice.

--- src/redux/slice/adventureSlice.js
+++ src/redux/slice/adventureSlice.js
@@ -1,9 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import fetchAdventuresData from "../adventureActions";
+import axios from "axios";
+import { useSelector } from 'react-redux';
 
 const adventuresInitialState = {
   adventures: [],
+  creationSuccess: false,
+  creationLoading: false,
+  creationError: false,
+  deletionSuccess: false,
+  deletionLoading: false,
+  deletionError: false,
 };
+const user_name = useSelector((state) => state.user.name);
+export const createAdventure = createAsyncThunk("adventure/create", async (username) => {
+  console.log('user name: ', user_name)
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:3000/api/v1/create_adventure",
+      {
+        user_name,
+        picture: 
+      }
+    );
+    return response.data; 
+  } catch (error) {
+    throw error.message; // Throwing error message on failure
+  }
+});
 
 const adventuresSlice = createSlice({
   name: "adventures",
