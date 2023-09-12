32cbea0982109b19355807d259adb92ebfc0bfb0
kalbek <access2kaleb@gmail.com>
2023-09-12 14:34:26 +0300

    Align search component on top of the page.

--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -4,24 +4,38 @@ import { FcSearch } from 'react-icons/fc';
 
 function SearchComponent({ myInputRef, searchAdventures }) {
   return (
-    <div className="row p-2">
-      <div className="input-group mt-2">
-        <input
-          type="text"
-          placeholder="Search for adventures"
-          className="form-control"
-          ref={myInputRef}
-        />
-        <button
-          className="btn
-          btn-outline-primary"
-          type="button"
-          onClick={searchAdventures}
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
+    <div className="position-absolute top-0 w-80">
+      <div className="row p-2">
+        <div
+          className="input-group mt-2"
+          style={{
+            minWidth: '60vw',
+            marginLeft: '20%',
+          }}
         >
-          <FcSearch />
-        </button>
+          <input
+            type="text"
+            placeholder="Search for adventures"
+            className="form-control"
+            ref={myInputRef}
+            style={{
+              outline: '#0d6efd solid 2px',
+            }}
+          />
+          <button
+            style={{
+              outline: '#0d6efd solid 2px',
+            }}
+            className="btn
+           "
+            type="button"
+            onClick={searchAdventures}
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <FcSearch />
+          </button>
+        </div>
       </div>
     </div>
   );
