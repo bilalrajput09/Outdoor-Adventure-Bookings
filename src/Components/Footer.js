1a8c97bff5b4124e5d726d1f9e9e489a70ba4f88
Bilal <ranabilalrajput09@gmail.com>
2023-09-13 12:23:03 +0200

    Fix eslint .

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,47 +1,5 @@
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
-const Footer = () => {
-  return (
-    <>
-      <footer className="bg-secondary text-white text-center py-3 mt-5 sticky-footer">
-        <div className="container">
-          <p>
-            &copy; Made with ❤ by
-            <Link
-              to="https://github.com/bilalrajput09"
-              className="link-warning"
-            >
-              {' Bilal Ahmed '}
-            </Link>
-            <Link
-              to="https://github.com/Daniel-Kigozi"
-              className="link-warning"
-            >
-              {' Daniel Kigozi '}
-            </Link>
-            <Link
-              to="https://github.com/kalbek"
-              className="link-warning"
-            >
-              {' Bekele Kaleb '}
-            </Link>
-          </p>
-          <p>
-            <Link to="/" className="text-white">
-              Privacy Policy
-            </Link>{' '}
-            |
-            <Link to="/" className="text-white">
-              Terms of Service
-            </Link>
-          </p>
-        </div>
-      </footer>
-    </>
-  );
-};
-=======
 const Footer = () => (
   <>
     <footer className="bg-secondary text-white text-center py-3 mt-5 sticky-footer">
@@ -79,6 +37,5 @@ const Footer = () => (
     </footer>
   </>
 );
->>>>>>> 208e49bfd4d242cc51821291ed4b46c6d5f5c1fe
 
 export default Footer;
